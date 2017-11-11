78a299838e70ea86556a43c4427bc4f7a1c03968
niklas <nyholmniklas@gmail.com>
2017-11-12 01:39:37 +0200

    Commited code from final video

--- src/main/java/org/niklas/rabbitmqtutorial/Common.java
+++
@@ -1,6 +0,0 @@
-package org.niklas.rabbitmqtutorial;
-
-public class Common {
-    public final static String QUEUE_NAME = "my-queue";
-    public final static String URI = "amqp://guest:guest@localhost";
-}
